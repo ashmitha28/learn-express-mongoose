ee8fd4f39f70de3c1b52a7a1d21b2ab2b9bb3268
Ashmitha <70202007+ashmitha28@users.noreply.github.com>
2024-03-18 16:12:15 -0400

    class

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,14 @@
 let BookInstance = require('../models/bookinstance');
 
 exports.show_all_books_status = function(res) {
-  return res.send([]);
-}
+  BookInstance.find({'status':{$eq: 'Available'}})
+      .populate('book')
+      .exec()
+      .then(list_bookinstances => {
+        res.send(list_bookinstances.map(function(BookInstance){
+          return BookInstance.book.title + " : "+ BookInstance.status
+        }));
+      })
+        .catch(err => res.send('status not found'));
+  
+};
